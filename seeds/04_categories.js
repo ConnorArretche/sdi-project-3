d220599931a4174674ec9f10b21e94ebca342b30
Connor Arretche <connor.r.arretche@gmail.com>
2026-07-01 10:30:03 -0500

    added seeded data for categories and transactions. hardcoded in categories as Faker did not have an option to seed random data i wanted

--- seeds/04_categories.js
+++ seeds/04_categories.js
@@ -1,13 +1,19 @@
+const { faker } = require('@faker-js/faker');
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
+
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
+  await knex('categories').del()
+  await knex('categories').insert([
+    { name: 'Food' },
+    { name: 'Groceries' },
+    { name: 'Travel' },
+    { name: 'Gas & Transit' },
+    { name: 'Shopping & Retail' },
+    { name: 'Utilities' },
+    { name: 'Entertainment' },
   ]);
 };
--- seeds/05_transactions.js
+++ seeds/05_transactions.js
@@ -1,13 +1,28 @@
+const { faker } = require('@faker-js/faker');
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
+
+function createTransactions(rows, accounts, categories){
+  let transactionData = [];
+  for(let i = 1; i <= rows; i++){
+    let randomAccount = accounts[Math.floor(Math.random()* accounts.length)];
+    let randomCategory = categories[Math.floor(Math.random() * categories.length)];
+    transactionData.push({amount: Number(faker.finance.amount()),
+                          description: faker.finance.transactionDescription(),
+                          time_stamp: faker.date.between({from: '2026-01-01', to : Date.now()}),
+                          account_id: randomAccount.id,
+                          category_id: randomCategory.id
+  })
+  }
+  return transactionData;
+}
+
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
-  ]);
+  await knex('transactions').del()
+  let accounts = await knex('accounts');
+  let categories = await knex('categories');
+  await knex('transactions').insert(createTransactions(15, accounts, categories));
 };
